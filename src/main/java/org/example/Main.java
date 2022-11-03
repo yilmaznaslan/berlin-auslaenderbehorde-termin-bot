ab9c315aa6e453627b8cb73e8a1c52f32d7259d0
Yilmaz Naci Aslan <yilmaznaci.aslan@maibornwolff.de>
2022-11-03 12:57:13 +0100

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,84 +1,164 @@
 package org.example;
 
+import okhttp3.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Scanner;
-import java.util.zip.GZIPInputStream;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        URL url = new URL("https://otv.verwalt-berlin.de/api/remote2/TerminBuchen/0a943678-fe4c-4d20-874e-227220d240fc/proceed?dswid=8502&suppressRenderOnChange=true");
-        HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
-        httpConn.setRequestMethod("POST");
+    public static String dswid;
+    public static String dsrid;
+    public static String requestId;
+    public static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
-        httpConn.setRequestProperty("Host", "otv.verwalt-berlin.de");
-        httpConn.setRequestProperty("Connection", "keep-alive");
-        httpConn.setRequestProperty("sec-ch-ua", "\"Google Chrome\";v=\"107\", \"Chromium\";v=\"107\", \"Not=A?Brand\";v=\"24\"");
-        httpConn.setRequestProperty("Accept", "*/*");
-        httpConn.setRequestProperty("Content-Type", "multipart/form-data; boundary=----WebKitFormBoundaryxYnOYUFCpb4ViT0Y");
-        httpConn.setRequestProperty("X-Requested-With", "XMLHttpRequest");
-        httpConn.setRequestProperty("sec-ch-ua-mobile", "?0");
-        httpConn.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36");
-        httpConn.setRequestProperty("sec-ch-ua-platform", "\"macOS\"");
-        httpConn.setRequestProperty("Origin", "https://otv.verwalt-berlin.de");
-        httpConn.setRequestProperty("Sec-Fetch-Site", "same-origin");
-        httpConn.setRequestProperty("Sec-Fetch-Mode", "cors");
-        httpConn.setRequestProperty("Sec-Fetch-Dest", "empty");
-        httpConn.setRequestProperty("Referer", "https://otv.verwalt-berlin.de/ams/TerminBuchen/wizardng/9890b240-2ce6-4b5b-9ca6-5efbb962c88c?v=1667318660115");
-        httpConn.setRequestProperty("Accept-Encoding", "gzip, deflate, br");
-        httpConn.setRequestProperty("Accept-Language", "en-GB,en-US;q=0.9,en;q=0.8");
-        httpConn.setRequestProperty("Cookie", "JSESSIONID=8hu7CQ-45MZ4J3r-bIy1C0kVP2XOiYpj5dmicNoj.frontend-1; SERVERID=frontend-1; otv_neu=d0f9ce66047a3e3090d61c1db32e6579; TS018ca6c6=01d33437f9990c606221e82381112ef6d9472f5c5d59e7c2734d1818a96e189e120a7f0ac3ddbf63c5b61cdbb389246d6cabefc544");
-
-        httpConn.setDoOutput(true);
-        OutputStreamWriter writer = new OutputStreamWriter(httpConn.getOutputStream());
-        writer.write("------WebKitFormBoundaryxYnOYUFCpb4ViT0Y\nContent-Disposition: form-data; name=\"applicationForm:managedForm\"\n\napplicationForm:managedForm\n------WebKitFormBoundaryxYnOYUFCpb4ViT0Y\nContent-Disposition: form-data; name=\"frid\"\n\n9d6c83fe-5f9a-472c-90ee-92f719fb12bb\n------WebKitFormBoundaryxYnOYUFCpb4ViT0Y\nContent-Disposition: form-data; name=\"lang\"\n\nde\n------WebKitFormBoundaryxYnOYUFCpb4ViT0Y\nContent-Disposition: form-data; name=\"gelesen\"\n\non\n------WebKitFormBoundaryxYnOYUFCpb4ViT0Y\nContent-Disposition: form-data; name=\"xima-9875-required\"\n\n\n------WebKitFormBoundaryxYnOYUFCpb4ViT0Y\nContent-Disposition: form-data; name=\"xfc-pp-elementslist\"\n\ngelesen\n------WebKitFormBoundaryxYnOYUFCpb4ViT0Y\nContent-Disposition: form-data; name=\"error_code\"\n\ne6:76gg4d0360hd73he0g303i<d;6if6gf<6\n------WebKitFormBoundaryxYnOYUFCpb4ViT0Y\nContent-Disposition: form-data; name=\"clientWizardStep\"\n\nMV9JbmZvcm1hdGlvbg==\n------WebKitFormBoundaryxYnOYUFCpb4ViT0Y\nContent-Disposition: form-data; name=\"applicationForm:managedForm:j_idt314\"\n\n\n------WebKitFormBoundaryxYnOYUFCpb4ViT0Y\nContent-Disposition: form-data; name=\"javax.faces.ViewState\"\n\n-7202582509904912962:-3739826161992955906\n------WebKitFormBoundaryxYnOYUFCpb4ViT0Y\nContent-Disposition: form-data; name=\"javax.faces.ClientWindow\"\n\n8502\n------WebKitFormBoundaryxYnOYUFCpb4ViT0Y--\n");
-        writer.flush();
-        writer.close();
-        httpConn.getOutputStream().close();
-
-        InputStream responseStream = httpConn.getResponseCode() / 100 == 2
-                ? httpConn.getInputStream()
-                : httpConn.getErrorStream();
-        if ("gzip".equals(httpConn.getContentEncoding())) {
-            responseStream = new GZIPInputStream(responseStream);
-        }
-        Scanner s = new Scanner(responseStream).useDelimiter("\\A");
-        String response = s.hasNext() ? s.next() : "";
-        System.out.println(response);
+    public static void main(String[] args) throws IOException, InterruptedException {
+        requestId = getRequestId();
+        activateRequestId(requestId);
+        fillTheForm();
     }
 
+    public static WebElement getElement(String elementName, WebDriver driver) throws InterruptedException {
+        while (true) {
+            try {
+                LOGGER.info("Looking for the element: {}", elementName);
+                return driver.findElement(By.name(elementName));
+            } catch (Exception e) {
+                LOGGER.warn(e.getLocalizedMessage());
+            }
 
-//String hostUrl = "https://otv.verwalt-berlin.de/ams/TerminBuchen/wizardng";
-//String requestId = "0eca6037-0ec1-4db0-8258-fb0678dcefbd";
-//       activateId(requestId);
-//String targetUrl = hostUrl + "/" + requestId +"?dswid=5224&dsrid=387&st=2&v="+getRandomNumber();
-//SessionCatcher.getServiceSelectionTab(targetUrl);
+            Thread.sleep(1000);
+        }
+    }
+
+    private static void setCitizenshipValue(WebDriver driver) throws InterruptedException {
+        WebElement element = getElement("sel_staat", driver);
+        if (element.isEnabled()) {
+            LOGGER.info("Element is enabled");
+            Select asd = new Select(element);
+            asd.selectByValue("163");
+        } else {
+            LOGGER.warn("Element is not enabled");
+        }
+    }
+
+    public static void fillTheForm() throws InterruptedException {
+
+        WebDriver driver = getServiceSelectionTab();
+        setCitizenshipValue(driver);
+    }
+
+    private static void activateRequestId(String requestId) throws IOException {
+
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("text/plain");
+        RequestBody body = new MultipartBody.Builder().setType(MultipartBody.FORM)
+                .addFormDataPart("hello", "myfriend")
+                .build();
+        Request request = new Request.Builder()
+                .url("https://otv.verwalt-berlin.de/api/remote2/TerminBuchen/" + requestId + "/proceed?dswid=8502&suppressRenderOnChange=true")
+                .method("POST", body)
+                .addHeader("Host", "otv.verwalt-berlin.de")
+                .addHeader("Connection", "keep-alive")
+                .addHeader("sec-ch-ua", "\"Google Chrome\";v=\"107\", \"Chromium\";v=\"107\", \"Not=A?Brand\";v=\"24\"")
+                .addHeader("Accept", "*/*")
+                .addHeader("X-Requested-With", "XMLHttpRequest")
+                .addHeader("sec-ch-ua-mobile", "?0")
+                .addHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36")
+                .addHeader("sec-ch-ua-platform", "\"macOS\"")
+                .addHeader("Origin", "https://otv.verwalt-berlin.de")
+                .addHeader("Sec-Fetch-Site", "same-origin")
+                .addHeader("Sec-Fetch-Mode", "cors")
+                .addHeader("Sec-Fetch-Dest", "empty")
+                .addHeader("Referer", "https://otv.verwalt-berlin.de/ams/TerminBuchen/wizardng/9890b240-2ce6-4b5b-9ca6-5efbb962c88c?v=1667318660115")
+                .addHeader("Accept-Encoding", "gzip, deflate, br")
+                .addHeader("Accept-Language", "en-GB,en-US;q=0.9,en;q=0.8")
+                .addHeader("Cookie", "JSESSIONID=8hu7CQ-45MZ4J3r-bIy1C0kVP2XOiYpj5dmicNoj.frontend-1; SERVERID=frontend-1; otv_neu=d0f9ce66047a3e3090d61c1db32e6579; TS018ca6c6=01d33437f9990c606221e82381112ef6d9472f5c5d59e7c2734d1818a96e189e120a7f0ac3ddbf63c5b61cdbb389246d6cabefc544; JSESSIONID=pEgF7e2XT7kaIpl7yHrZAndVWYt1dIzIwpgFdw62.frontend-1; SERVERID=frontend-2; TS018ca6c6=01d33437f9832280903ece3a5244806bacae79dae315b0c3012bf2c5c185ecce30ff0fd9c7a44fc5ba29bf6e8b2e41c30f468e49ef; otv_neu=d0f9ce66047a3e3090d61c1db32e6579; JSESSIONID=yT4lc5LxUWUzmqMwiYUXJqv7G6sypQaWyLnrke6d.frontend-1; TS018ca6c6=01d33437f99f6cf591632be481b881df35db07d620fe6cdf8ad9f23c8cb9224c2de7c285120642136301818243edb836a1f6b3bf0b")
+                .build();
+        Response response = client.newCall(request).execute();
+        System.out.println("RresponseCode; " + response.code());
+    }
+
+    public static WebDriver getServiceSelectionTab() {
+        String hostUrl = "https://otv.verwalt-berlin.de/ams/TerminBuchen/wizardng";
+        String targetUrl = hostUrl + "/" + requestId + "?dswid=" + dswid + "&dsrid=" + dsrid;
+        ChromeOptions options = new ChromeOptions();
+        //options.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(options);
+        LOGGER.info("Getting the URL: {}", targetUrl);
+        driver.get(targetUrl);
+        return driver;
+    }
+
+    public static String getRequestId() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(options);
+
+        String requestId;
+        String initialUrl = "https://otv.verwalt-berlin.de/ams/TerminBuchen/wizardng";
+        LOGGER.info("Getting the URL: {}", initialUrl);
+
+        driver.get(initialUrl);
+        while (true) {
+            String urlAfterRedirect = driver.getCurrentUrl();
+            LOGGER.info("CurrentURL: " + urlAfterRedirect);
+            try {
+                URL url = new URL(urlAfterRedirect);
+                String queryStr = url.getQuery();
+                if (dsrid == null && dswid == null) {
+                    LOGGER.info("QueryString: {}", queryStr);
+                    setDs(queryStr);
+                }
+
+                if (urlAfterRedirect.contains("?v")) {
+                    requestId = extractRequestId(urlAfterRedirect);
+                    driver.close();
+                    break;
+                }
 
 
-public static int getRandomNumber(){
-        int min=100;
-        int max=999999999;
-        return(int)((Math.random()*(max-min))+min);
+            } catch (MalformedURLException e) {
+                LOGGER.error("URL is malformed exception occurred", e);
+            }
         }
 
-private static void activateId(String requestId)throws IOException{
-        URL url=new URL("https://otv.verwalt-berlin.de/api/remote2/TerminBuchen/"+requestId+"/proceed?dswid=8502&suppressRenderOnChange=true");
-        System.out.println("URL:"+url.toString());
-        HttpURLConnection http=(HttpURLConnection)url.openConnection();
-        http.setRequestMethod("POST");
-        http.setDoOutput(true);
-        http.setRequestProperty("Host","otv.verwalt-berlin.de");
-        http.setRequestProperty("Connection","keep-alive");
-        //http.setRequestProperty("sec-ch-ua", ""Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"");
-        http.setRequestProperty("Accept","*/*");
-        http.setRequestProperty("Content-Type","multipart/form-data; boundary=----WebKitFormBoundaryxYnOYUFCpb4ViT0Y");
-        http.setRequestProperty("X-Requested-With","XMLHttpRequest");
-        http.setRequestProperty("sec-ch-ua-mobile","?0");
-        http.setRequestProperty("User-Agent","Mozilla/5.0 (Macint)");
-        }
+        return requestId;
+    }
+
+    public static void setDs(String queryStr) {
+        List<String> queryStrings = List.of(queryStr.split("&"));
+        dsrid = Arrays.stream(queryStrings.get(0).split("=")).toList().get(1);
+        dswid = Arrays.stream(queryStrings.get(1).split("=")).toList().get(1);
+        LOGGER.info("dswid: {}, dsrid: {}", dswid, dsrid);
+    }
+
+    public static String extractRequestId(String url) {
+        List<String> urlAsList = List.of(url.split("/"));
+        String requestIdAndV = urlAsList.get(urlAsList.size() - 1);
+
+        String requestId = List.of(requestIdAndV.split("\\?")).get(0);
+
+        //dsrid = Arrays.stream(queryStrings.get(0).split("=")).toList().get(1);
+        //dswid = Arrays.stream(queryStrings.get(1).split("=")).toList().get(1);
+        LOGGER.info("RequestID: {}", requestId);
+        //LOGGER.info("RequestId: {}, v: {}", dswid, dsrid);
+        return requestId;
+
+    }
+
 
 }
