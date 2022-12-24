073ce900d6d96a896af789e77fd43e5f893826c1
Yilmaz Naci Aslan <yilmaznaci.aslan@maibornwolff.de>
2022-12-24 10:58:18 +0100

    improve notification

--- src/main/java/org/example/notifications/TwilioAdapter.java
+++ src/main/java/org/example/notifications/TwilioAdapter.java
@@ -2,6 +2,9 @@ package org.example.notifications;
 
 import com.twilio.rest.api.v2010.account.Call;
 import com.twilio.rest.api.v2010.account.Message;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.net.URI;
 
 /**
@@ -12,27 +15,36 @@ public class TwilioAdapter {
     public static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
     public static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
     public static final String PHONE_NUMBER_FROM = System.getenv("TWILIO_PHONE_FROM");
+    private static final Logger logger = LogManager.getLogger(TwilioAdapter.class);
 
     private TwilioAdapter() {
     }
 
     public static void makeCall(String phoneNumberToBeNotified) {
         com.twilio.Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
-        Call.creator(
-                        new com.twilio.type.PhoneNumber(phoneNumberToBeNotified),
-                        new com.twilio.type.PhoneNumber(PHONE_NUMBER_FROM),
-                        URI.create("http://demo.twilio.com/docs/voice.xml"))
-                .create();
+        try{
+            Call.creator(
+                            new com.twilio.type.PhoneNumber(phoneNumberToBeNotified),
+                            new com.twilio.type.PhoneNumber(PHONE_NUMBER_FROM),
+                            URI.create("http://demo.twilio.com/docs/voice.xml"))
+                    .create();
+        } catch (Exception e){
+            logger.warn("Failed to make Call", e);
+        }
 
     }
 
     public static void sendSMS(String phoneNumberToBeNotified, String msgBody){
         com.twilio.Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
-        Message.creator(
-                        new com.twilio.type.PhoneNumber(phoneNumberToBeNotified),
-                        new com.twilio.type.PhoneNumber(PHONE_NUMBER_FROM),
-                        msgBody)
-                .create();
+        try{
+            Message.creator(
+                            new com.twilio.type.PhoneNumber(phoneNumberToBeNotified),
+                            new com.twilio.type.PhoneNumber(PHONE_NUMBER_FROM),
+                            msgBody)
+                    .create();
+        }catch (Exception e){
+        logger.warn("Failed to send SMS", e);
+        }
 
     }
 
