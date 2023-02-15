bc517b88c5e81fe3a8af792db77d427ffc239e1b
Yilmaz Naci Aslan <yilmaznaci.aslan@maibornwolff.de>
2023-02-15 21:21:31 +0100

    delete not used class

--- src/main/java/org/example/auslanderbehorde/formfiller/model/FormAllDTO.java
+++
@@ -1,5 +0,0 @@
-package org.example.auslanderbehorde.formfiller.model;
-
-public record FormAllDTO(Section2FormInputs section2FormInputs,
-                         Section4FormInputs section4FormInputs) {
-}
